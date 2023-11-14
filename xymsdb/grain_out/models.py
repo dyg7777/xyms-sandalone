ec5ca78af36821f696fbcdc9052b5fe3f8656ba4
xykc <xykc@xy2cn.cn>
2023-11-14 21:50:57 +0800

    up

--- xymsdb/grain_out/models.py
+++ xymsdb/grain_out/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+# 货权人信息
+class OwnerForGoods(models.Model):
+    ownerforgoods_name=models.CharField(max_length=255,blank=False,null=True,default='企业私有',help_text='货权人名称')
+    ownerforgoods_uncode=models.CharField(max_length=100,blank=False,null=True,default='',help_text='货权人代码')
+    ownerforgoods_phonenumber=models.CharField(max_length=100,blank=False,null=True,default='',help_text='货权人联系方式')
+    co_owners=models.CharField(max_length=255,blank=False,null=True,help_text='货物共有人')
--- xymsdb/login/models.py
+++ xymsdb/login/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import make_password
+from django.contrib.auth.hashers import make_password,check_password
 from django.utils import timezone
 # Create your models here.
 
-
+# 企业信息和授权信息
 class Enterprise(models.Model):
     enter_name=models.CharField(max_length=255,null=True,blank=False,default='',help_text='企业名称')
     enter_uncode = models.CharField(max_length=255,null=True,blank=False, default='', help_text='统一代码证号')
@@ -13,12 +13,15 @@ class Enterprise(models.Model):
     enter_author_code = models.CharField(max_length=255,null=True,blank=False, default="", help_text='授权码')
     enter_free_credit = models.IntegerField(max_length=5, null=True, blank=False,default=50,help_text='授权额度')
 
+
+# 作用于软件内部的时间
 class RunDate(models.Model):
     run_date=models.DateField(default=timezone.datetime.now,help_text='系统执行时间')
 
 
-
-
-class User(models.Model):
+# 用户权限
+class UserPermissions(models.Model):
     username=models.CharField(max_length=255,null=True,blank=False,default='鑫奕科创',help_text='用户名')
-    password = models.CharField(max_length=255, null=True,blank=False,default=make_password('15145181511'), help_text='登录密码')
+    password = models.CharField(max_length=255, null=True,blank=False,default=make_password('15145181511','1975217','pbkdf2_sha1'), help_text='登录密码')
+    
+
