2d3bd5166b6aac1d335f20ac3453f6fdeefff763
xykc <xykc@xy2cn.cn>
2023-11-30 15:28:01 +0800

    up

--- xymsdb/login/urls.py
+++ xymsdb/login/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from .views import ResetDefault
 
 urlpatterns = [
-    path(r'set_default/',ResetDefault.set_login_default),
-    path(r'get_enterprise_information/',ResetDefault.get_enterprise_information),
+    path(r'set_default/', ResetDefault.set_login_default),
+    path(r'get_enterprise_information/',
+         ResetDefault.get_enterprise_information),
 ]
