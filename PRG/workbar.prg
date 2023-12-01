3c5400a687d82ebe525ea6ff5ff9243dc1fd717c
xykc <xykc@xy2cn.cn>
2023-12-01 17:23:22 +0800

    up

--- PRG/workbar.prg
+++ PRG/workbar.prg
@@ -1,7 +1,1 @@
-SET MESSAGE TO '应用软件授权单位：'+DW_1+SPACE(2)+'收购单位税务登记号：'+SWDJ_1+SPACE(5)+'软件版本号：'+ALLTRIM(BBH_1)+SPACE(5)+'软件授权使用时间：'+ALLTRIM(DTOC(SQRQ_1))+SPACE(5)+'鑫奕科技引领粮食收储管理软件先锋。服务电话：13009854012  QQ:50037071'
-IF SQRQ_1=DATE() THEN
-	=MESSAGEBOX('授权最后日期，请及时联系供应商，以免影响正常使用软件',0,'授权提示')
-ENDIF
-IF SQRQ_1<DATE() THEN
-	=MESSAGEBOX('授权超期，请联系软件供应商，或直接联系软件制造商--鑫奕科技',0,'授权提示')
-ENDIF
+SET MESSAGE TO '应用软件授权单位：'+ENTER_NAME+SPACE(2)+'收购单位税务登记号：'+ENTER_UNCODE+SPACE(5)+'软件版本号：'+VERSION_NUM+SPACE(5)+'软件授权使用时间：'+DTOC(ENTER_START)+'至'+DTOC(ENTER_END)+SPACE(5)+'鑫奕科技引领粮食收储管理软件先锋。服务电话：15145181511(微信同号) QQ:50037071'
--- xymsdb/login/views.py
+++ xymsdb/login/views.py
@@ -17,11 +17,11 @@ from .models import Enterprise, login_logs
 class ResetDefault():
 
     def set_enterprise_information(request):
-        data=json.loads(request.body)
+        data = json.loads(request.body)
         return HttpResponse('ok')
 
-
     # 获得企业信息
+
     def get_enterprise_information(request):
         data = json.loads(request.body)
         localuuid = data['uuid']
@@ -30,18 +30,18 @@ class ResetDefault():
             login_uuid=localuuid, return_uuid=retuuid)
         retsql.save()
         retsql = Enterprise.objects.all()
+        data1 = []
         data = []
         for ll in retsql.values():
             res = {
                 'enter_name': ll.get('enter_name'),
-                'enter_uncode':ll.get('enter_uncode'),
-                'enter_author_start_date': datetime.strftime(ll.get('enter_author_start_date'),"%Y-%m-%d %H:%M:%S"),
-                'enter_author_end_date': datetime.strftime(ll.get('enter_author_end_date'),"%Y-%m-%d %H:%M:%S"),
-                'enter_free_credit':ll.get('enter_free_credit'),
-                'version_number':ll.get('version_number'),
-                'app_name':ll.get(' app_name'),
+                'enter_uncode': ll.get('enter_uncode'),
+                'enter_author_start_date': datetime.strftime(ll.get('enter_author_start_date'), "%Y-%m-%d,%H:%M:%S"),
+                'enter_author_end_date': datetime.strftime(ll.get('enter_author_end_date'), "%Y-%m-%d,%H:%M:%S"),
+                'enter_free_credit': ll.get('enter_free_credit'),
+                'version_number': ll.get('version_number'),
+                'app_name': ll.get('app_name'),
             }
             data.append(res)
         ret_data = json.dumps(data)
-        print (data)
         return HttpResponse(content=ret_data, content_type='applicaton/json')
